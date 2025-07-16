e1e4ea93f7fd455eb65580291960f53a113a2fcc
Pedro H. Deluca <pedro.deluca@bradesco.com.br>
2025-07-15 22:06:23 -0300

    Consistencia

---
+++ Classes_Objetos/consistencia/PersonagemGame.java
@@ -0,0 +1,57 @@
+public class PersonagemGame {
+
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public PersonagemGame(int saudeAtualInicial, String nomeInicial) {
+        setSaudeAtual(saudeAtualInicial);
+        setNome(nomeInicial);
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+
+        if (this.saudeAtual > 0) {
+            this.status = "vivo";
+        } else {
+            this.status = "morto";
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome != null) {
+            if (!nome.isBlank()) {
+                this.nome = nome;
+            }
+        }
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        if (getSaudeAtual() - quantidadeDeDano >= 0) {
+            setSaudeAtual(getSaudeAtual() - quantidadeDeDano);
+        } else {
+            setSaudeAtual(0);
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        if (getSaudeAtual() + quantidadeDeCura <= 100) {
+            setSaudeAtual(getSaudeAtual() + quantidadeDeCura);
+        } else {
+            setSaudeAtual(100);
+        }
+    }
+}
