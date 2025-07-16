5ff204d9df7309db1f3ee3e0ec7af0e22b6b7a1d
Pedro H. Deluca <pedro.deluca@bradesco.com.br>
2025-07-16 19:44:44 -0300

    6. Classe Abstrata

---
+++ Heranca/abstrata/Retangulo.java
@@ -0,0 +1,37 @@
+public class Retangulo extends FormaGeometrica{
+
+    protected double largura;
+    protected double altura;
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        if (largura < 0) {
+            throw new IllegalArgumentException("Largura deve ser maior ou igual a 0");
+        }
+        this.largura = largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        if (altura < 0) {
+            throw new IllegalArgumentException("Altura deve ser maior ou igual a 0");
+        }
+        this.altura = altura;
+    }
+
+    @Override
+    public double area() {
+        return getAltura() * getLargura();
+    }
+
+    @Override
+    public String toString() {
+        return "[Retangulo] " + String.format("%.2f", getLargura()) + " / " + String.format("%.2f", getAltura());
+    }
+}
