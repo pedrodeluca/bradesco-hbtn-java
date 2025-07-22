d96e67b05454672f290ec609b2f3ebc3a259c80e
Pedro H. Deluca <pedro.deluca@bradesco.com.br>
2025-07-22 18:24:41 -0300

    0. Divisao inteira

---
+++ 0x02/divisao/Numero.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+
+public class Numero {
+
+    public static void dividir(int a, int b) {
+        int resultado = 0;
+
+        try {
+             resultado = a / b;
+        } catch (Exception e) {
+            System.out.println("Nao eh possivel dividir por zero");
+        } finally {
+            System.out.println(a + " / " + b + " = " + resultado);
+        }
+    }
+}
