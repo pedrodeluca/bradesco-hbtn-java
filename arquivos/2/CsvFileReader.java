2cbf90866c1095a9344674281b015cc5ecfe522e
Pedro H. Deluca <pedro.deluca@bradesco.com.br>
2025-07-27 16:57:56 -0300

    2. Leitura de Arquivo CSV

---
+++ arquivos/2/CsvFileReader.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class CsvFileReader {
+    public static void main(String[] args) {
+        String path = "C:\\Desenv\\Projetos\\Holberton\\bradesco-hbtn-java\\arquivos\\2\\funcionarios.csv";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String linha;
+            while((linha = reader.readLine()) != null) {
+                String[] info = linha.split(",");
+                System.out.println("Funcionário: " + info[0]);
+                System.out.println("Idade: " + info[1]);
+                System.out.println("Departamento: " + info[2]);
+                System.out.println("Salarial: " + info[3]);
+                System.out.println("------------------------");
+            }
+            System.out.print("Leitura do arquivo concluída.");
+        } catch (Exception e) {
+            //System.out.println("Erro na leitura do arquivo!");
+        }
+
+    }
+}
