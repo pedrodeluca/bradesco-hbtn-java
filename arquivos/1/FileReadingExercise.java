21fe544dee0990a4c03bcfc95d3f4f45c4102e20
Pedro H. Deluca <pedro.deluca@bradesco.com.br>
2025-07-27 16:21:55 -0300

    1. Leitura de arquivos

---
+++ arquivos/1/FileReadingExercise.java
@@ -0,0 +1,24 @@
+import javax.annotation.processing.Filer;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class FileReadingExercise {
+    public static void main(String[] args) {
+        String path = "C:\\Desenv\\Projetos\\Holberton\\bradesco-hbtn-java\\arquivos\\1\\exemplo.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String linha;
+            System.out.println("Conteúdo do arquivo 'exemplo.txt': \n");
+            while((linha = reader.readLine()) != null) {
+                System.out.println(linha);
+            }
+            System.out.println("\nLeitura do arquivo concluída.");
+        } catch (Exception e) {
+            System.out.println("Erro na leitura do arquivo!");
+        }
+
+    }
+}
