cffe07eccb17ce0ca04e9fb62a00df7777f311d7
Pedro H. Deluca <pedro.deluca@bradesco.com.br>
2025-07-27 15:14:40 -0300

    0. Escrevendo Arquivos

---
+++ arquivos/0/FileWritingExercise.java
@@ -0,0 +1,40 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+
+public class FileWritingExercise {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita o nome do arquivo
+        System.out.print("Digite o nome do arquivo (com extensão .txt): ");
+        String fileName = scanner.nextLine();
+        String path = "C:\\Desenv\\Projetos\\Holberton\\bradesco-hbtn-java\\arquivos\\0\\" + fileName;
+
+        // implemente o codigo aqui
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+            registraDados(scanner, writer);
+        } catch (IOException e) {
+            System.out.println("Erro ao realizar a gravação do arquivo");
+        }
+
+        scanner.close();
+    }
+
+    public static void registraDados(Scanner scanner, BufferedWriter writer) throws IOException {
+        String conteudo="sim";
+        while (!conteudo.equals("sair")) {
+            System.out.print("Oque deseja gravar no arquivo: ");
+            conteudo = scanner.nextLine();
+
+            if (conteudo.equals("sair"))
+                return;
+
+            writer.write(conteudo);
+            writer.newLine();
+        }
+    }
+}
