23f520377af2755f2311b134f4cd8d2f539090ee
Pedro H. Deluca <pedro.deluca@bradesco.com.br>
2025-07-27 16:40:28 -0300

    Ajustando saida

--- arquivos/1/FileReadingExercise.java
+++ arquivos/1/FileReadingExercise.java
@@ -11,11 +11,11 @@ public class FileReadingExercise {
 
         try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
             String linha;
-            System.out.println("Conteúdo do arquivo 'exemplo.txt':");
+            System.out.println("Conteúdo do arquivo 'exemplo.txt':\n");
             while((linha = reader.readLine()) != null) {
                 System.out.println(linha);
             }
-            System.out.print("Leitura do arquivo concluída.");
+            System.out.print("\nLeitura do arquivo concluída.");
         } catch (Exception e) {
             //System.out.println("Erro na leitura do arquivo!");
         }
