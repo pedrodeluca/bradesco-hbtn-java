64e0407e5ae4195e4594a2a269180c6e70b453e0
Pedro H. Deluca <pedro.deluca@bradesco.com.br>
2025-07-28 05:34:15 -0300

    0. Array

--- estruturas_de_dados/0/ArrayExercicio.java
+++ estruturas_de_dados/0/ArrayExercicio.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ArrayExercicio {
+    public static void main(String[] args) {
+        // Criando o scanner para entrada de dados
+        Scanner scanner = new Scanner(System.in);
+
+        // Definindo o tamanho do array
+        int tamanhoArray = 10;
+        int[] numeros = new int[tamanhoArray];
+        int soma = 0;
+        int maiorNumero = Integer.MIN_VALUE;
+
+        // Preenchendo o array com números fornecidos pelo usuário
+        // Utilize scanner.nextInt(); para receber o numero digitado
+        System.out.println("Digite 10 números inteiros:");
+
+        for (int i=0 ; i<10 ; i++) {
+            int posicao=i+1;
+            System.out.print("Digite o número " + posicao + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+
+        // Exibindo os resultados
+        System.out.println("\n");
+        System.out.println("Conteúdo do array:");
+        for (int numero : numeros) {
+            System.out.print(numero + " ");
+            soma += numero;
+            if (maiorNumero < numero)
+                maiorNumero = numero;
+        }
+        System.out.println("\nSoma de todos os números: " + soma);
+        System.out.print("Maior número no array: " + maiorNumero);
+
+        // Fechando o scanner
+        scanner.close();
+    }
 }
