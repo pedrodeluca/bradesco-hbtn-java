9d423a3ba9bbd9ce3ff3806905448cbe1c8d267c
Pedro H. Deluca <pedro.deluca@bradesco.com.br>
2025-07-27 21:47:18 -0300

    3. Gerenciamento de Estoque com Arquivos

---
+++ arquivos/3/Estoque.java
@@ -0,0 +1,105 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Estoque {
+    public final String arquivo = "C:\\Desenv\\Projetos\\Holberton\\bradesco-hbtn-java\\arquivos\\3\\estoque.csv";
+
+    public Estoque(String s) {
+    }
+
+    public void adicionarProduto(String nome, int quantidade, double preco) {
+        List<Produto> produtos = lerAquivoCompleto();
+        Produto produtoNovo = new Produto(produtos.size() + 1, nome, quantidade, preco);
+        produtos.add(produtoNovo);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(arquivo))) {
+            for (Produto produto : produtos) {
+                writer.write(produto.getId() + "," + produto.getNome() + "," +
+                        produto.getQuantidade() + "," + produto.getPreco());
+                writer.newLine();
+            }
+            writer.close();
+            System.out.println("Lista gravada com sucesso no arquivo: " + arquivo);
+        } catch (IOException e) {
+            System.err.println("Ocorreu um erro ao gravar o arquivo: " + e.getMessage());
+        }
+    }
+
+    public void excluirProduto(int idExcluir) {
+        List<Produto> produtos = lerAquivoCompleto();
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(arquivo))) {
+            for (Produto produto : produtos) {
+                if (produto.getId() != idExcluir) {
+                    writer.write(produto.getId() + "," + produto.getNome() + "," +
+                            produto.getQuantidade() + "," + produto.getPreco());
+                    writer.newLine();
+                }
+            }
+            writer.close();
+            System.out.println("Lista gravada com sucesso no arquivo: " + arquivo);
+        } catch (IOException e) {
+            System.err.println("Ocorreu um erro ao gravar o arquivo: " + e.getMessage());
+        }
+    }
+
+    public void exibirEstoque() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(arquivo))) {
+            String linha;
+            while((linha = reader.readLine()) != null) {
+                String[] info = linha.split(",");
+                Produto produto = new Produto(Integer.parseInt(info[0]), info[1], Integer.parseInt(info[2]), Double.parseDouble(info[3]));
+                System.out.println(produto);
+            }
+        } catch (Exception e) {
+            System.out.println("Erro na leitura do metodo exibirEstoque");
+        }
+    }
+
+    public void atualizarQuantidade(int idAtualizar, int novaQuantidade) {
+        boolean idEncontrado=false;
+        Produto produtoAlterado = null;
+        List<Produto> produtos = lerAquivoCompleto();
+
+        for (int i=0 ; i<produtos.size() ; i++) {
+            if (idAtualizar == produtos.get(i).getId()) {
+                produtos.get(i).setQuantidade(novaQuantidade);
+                idEncontrado = true;
+                produtoAlterado = produtos.get(i);
+                break;
+            }
+        }
+
+        if (idEncontrado) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(arquivo))) {
+                for (Produto produto : produtos) {
+                    writer.write(produto.getId() + "," + produto.getNome() + "," +
+                            produto.getQuantidade() + "," + produto.getPreco());
+                    writer.newLine();
+                }
+                writer.close();
+                System.out.println("Lista gravada com sucesso no arquivo: " + arquivo);
+            } catch (IOException e) {
+                System.err.println("Ocorreu um erro ao gravar o arquivo: " + e.getMessage());
+            }
+        } else {
+            adicionarProduto(produtoAlterado.getNome(), produtoAlterado.getQuantidade(), produtoAlterado.getPreco());
+        }
+    }
+
+    public List<Produto> lerAquivoCompleto()  {
+        List<Produto> produtos = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(arquivo))) {
+            String linha;
+            while((linha = reader.readLine()) != null) {
+                String[] info = linha.split(",");
+                Produto produto = new Produto(Integer.parseInt(info[0]), info[1], Integer.parseInt(info[2]), Double.parseDouble(info[3]));
+                produtos.add(produto);
+            }
+        } catch (Exception e) {
+            System.out.println("Erro na leitura do metodo lerArquivoCompleto");
+        }
+        return produtos;
+    }
+}
