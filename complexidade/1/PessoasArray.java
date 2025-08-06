b4ea0366cd66a39fc1e54b369f47ad882d25fb49
Pedro H. Deluca <pedro.deluca@bradesco.com.br>
2025-08-06 13:28:06 -0300

    Ajustando saida do exercicio 1

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -20,9 +20,7 @@ public class PessoasArray {
     // implementar o método de buscaLinear
     public void buscaLinear(String nome){
         boolean encontrado = false;
-
         System.out.println("Procurando pelo nome: \"" + nome + "\"");
-
         for (int i=0 ; i<nomes.length ; i++) {
             System.out.println("Passando pelo indice:" + i);
             if (nomes[i].equals(nome)) {
@@ -30,7 +28,6 @@ public class PessoasArray {
                 encontrado = true;
             }
         }
-
         if (!encontrado) {
             throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
         }
