dd4ea0afea028a50f508bc418cdf58e9844d057d
Pedro H. Deluca <pedro.deluca@bradesco.com.br>
2025-08-06 17:29:25 -0300

    a

--- arquivos/2/CsvFileReader.java
+++ arquivos/2/CsvFileReader.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class CsvFileReader {
     public static void main(String[] args) {
-        String path = "C:\\Desenv\\Projetos\\Holberton\\bradesco-hbtn-java\\arquivos\\2\\funcionarios.csv";
+        String path = "C:/Desenv/Projetos/Holberton/bradesco-hbtn-java/arquivos/2/funcionarios.csv";
 
         try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
             String linha;
