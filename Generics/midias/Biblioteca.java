a87d1d68a7ccd000768792ac316033b4c10d6792
Pedro H. Deluca <pedro.deluca@bradesco.com.br>
2025-08-07 14:19:21 -0300

    Ajustando extensao do generics

--- Generics/midias/Biblioteca.java
+++ Generics/midias/Biblioteca.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Biblioteca<Midia>{
+public class Biblioteca<T> extends Midia {
     List<Midia> lista = new ArrayList<>();
 
     public void adicionarMidia(Midia dados) {
