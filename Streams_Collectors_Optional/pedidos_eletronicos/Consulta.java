7f1b2afce4a0f33767f01efe9fe59de4a15adcaf
Pedro H. Deluca <pedrodluk@gmail.com>
2025-08-29 19:23:02 +0000

    subindo exercicio

---
+++ Streams_Collectors_Optional/pedidos_eletronicos/Consulta.java
@@ -0,0 +1,36 @@
+import java.util.List;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        List<Produto> livros = pedido.getProdutos().stream()
+                .filter(p -> p.getCategoria() == CategoriaProduto.LIVRO)
+                .toList();
+
+        return livros;
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        Produto produtoMaiorPreco = produtos.stream()
+                .max((p1, p2) -> Double.compare(p1.getPreco(), p2.getPreco())
+        ).orElse(null);
+
+        return produtoMaiorPreco;
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double precoMinimo) {
+        List<Produto> produtoPrecoMinimo = produtos.stream()
+                .filter(p -> p.getPreco() >= precoMinimo)
+                .toList();
+
+        return produtoPrecoMinimo;
+    }
+
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos) {
+        List<Pedido> pedidosEletronicos = pedidos.stream()
+                .filter(p -> p.getProdutos().stream()
+                        .anyMatch(produto -> produto.getCategoria() == CategoriaProduto.ELETRONICO))
+                .toList();
+
+        return pedidosEletronicos;
+    }
+}
